9940b07eaa36ca98c3aafee603775d79a8c5748f
simple-parser <goshzimma@gmail.com>
2023-10-03 17:33:38 -0700

    done

---
+++ wp-includes/blocks/navigation/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'efa7b39e8a2ac7d3f83a');
---
+++ wp-includes/blocks/navigation/view.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'dde7635cad8b0819b900');
